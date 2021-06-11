03c8571f1111069a288398f383c68e76d1036350
ernesto galarza <ernestoagc87@gmail.com>
2021-06-11 02:12:00 -0500

    update configuration propertios

--- src/main/java/com/fplusf/message/MessageApiApplication.java
+++ src/main/java/com/fplusf/message/MessageApiApplication.java
@@ -4,10 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessageApiApplication {
+public class MessageApiApplication implements Runnable {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MessageApiApplication.class, args);
+	}
+
+	@Override
+	public void run() {
+		System.out.println("============>");
+		System.out.println("============>");
+		System.out.println("============>CCC");
+		System.out.println("============>CCC");
 	}
 
 }
